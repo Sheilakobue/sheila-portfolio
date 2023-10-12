18591f21c971cad8f3ef8da961b5c4304f1df9aa
Sheila Kobue <maserole.mojela@gmail.com>
2023-10-12 09:55:57 +0200

    changed image

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,44 +1,56 @@
 //import React from "react";
+import React from "react";
 import "./About.css";
 import Me from "../../Asset/me-about.png";
 import { FaAward, FaUsers, FaFolderOpen } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHtml5,
+  faCss3,
+  faJs,
+  faGithub,
+  faGit,
+  faReact,
+  faNodeJs,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function About() {
   return (
     <section id="about">
-      <h5>Get to know</h5>
-      <h2>About Me</h2>
-
-      <div className="about_card_container">
-        <div className="about_card">
-          <div className="about_me">
-            <div className="about_me.image">
-              <img src={Me} alt="About Image" />
-            </div>
-          </div>
-
-          <div className="about_text">
-            <p>
-              I am an enthusiastic, resourceful, self-motivated, reliable,
-              responsible, and hard-working person. I am adaptable to all
-              challenging situations, I can work well both in a team environment
-              as well as using my initiative. I can work well under pressure and
-              adhere to strict deadlines.
-            </p>
-          </div>
-
-          <a href="#contact" className="btn btn.primary">
-            Let's Talk
-          </a>
-        </div>
-      </div>
-
+      {/* ... (rest of your code) */}
       <div className="about_content">
         <div className="about_card">
           <article className="about_card">
             <FaAward className="about_icon" />
-            <h5>Experience</h5>
-            <small>1 Year Learning</small>
+            {/* ... (rest of your code) */}
+            
+            <h5>Languages</h5>
+            <div className="iconContainer">
+              <a href="#" className="iconLinks">
+                <FontAwesomeIcon icon={faHtml5} />
+              </a>
+
+              <a href="#" className="iconLinks">
+                <FontAwesomeIcon icon={faCss3} />
+              </a>
+
+              <a href="#" className="iconLinks">
+                <FontAwesomeIcon icon={faJs} />
+              </a>
+            </div>
+
+            <h5>Frameworks</h5>
+            <div className="iconContainer">
+              <a href="#" className="iconLinks">
+                <FontAwesomeIcon icon={faReact} />
+              </a>
+
+              <a href="#" className="iconLinks">
+                <FontAwesomeIcon icon={faNodeJs} />
+              </a>
+            </div>
+          </article>
+            </div>
           </article>
 
           <article className="about_card">
